f9076560b516d3381e934c0bac0bb59de724e708
djp <djp@gmail.com>
2024-03-26 16:20:08 +0900

    backend springboot with gradle

---
+++ backend/src/src/main/java/com/wfl/test/WfltestApplication.java
@@ -0,0 +1,13 @@
+package com.wfl.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfltestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WfltestApplication.class, args);
+	}
+
+}
---
+++ backend/src/src/test/java/com/wfl/test/WfltestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wfl.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WfltestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
