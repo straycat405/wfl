f37370a1a870621131a46b2837b15deafef997ac
djp <djp@gmail.com>
2024-03-26 16:24:37 +0900

    frontend commit with react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import GroupList from "./GroupList";
+import GroupEdit from "./GroupEdit";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/groups" exact={true} element={<GroupList />} />
+		<Route path='/groups/:id' element={<GroupEdit />} />
+      </Routes>
+    </Router>
+  );
+}
